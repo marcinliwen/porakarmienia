d0878ad1c67d4e6431db382ab536136d535f66b9
marcinliwen <liwen.marcin@gmail.com>
2019-06-07 22:52:15 +0200

    add next and js functions

--- main.js
+++ main.js
@@ -34,12 +34,14 @@ function getCurrentTime(){
     
     stopFeedBtn.addEventListener('click', function(){
         var today = new Date();
+        var nextTime = today.getTime();
         var hours = today.getHours();
         var minutes = today.getMinutes();
         hours = (hours < 10) ? '0'+ hours : hours;
         minutes = (minutes < 10 ) ? '0'+ minutes : minutes;
         var time = hours+':'+minutes;
         createTimeStop(time);
+        setTimeNext(nextTime, 2);
 
         this.classList.add('disable');
         startFeedBtn.classList.remove('disable');
@@ -53,13 +55,35 @@ function createTimeStart(date, time){
     document.querySelector('.time-mark .-left .--to').innerHTML = "do";
     document.querySelector('.time-mark .-left .--to').classList.add('waiting');
     document.querySelector('.time-mark  .--date').innerHTML = date;
-    document.querySelector('.time-mark').classList.add('active');
+    document.querySelector('#js-last').classList.remove('inactive');
 }
 
 function createTimeStop(time){
     document.querySelector('.time-mark .-left .--to').innerHTML = time;
     document.querySelector('.time-mark .-left .--to').classList.remove('waiting');
+    document.querySelector('#js-next').classList.remove('inactive');
 }
 
+function setTimeNext(currentTime, interval){
+    var timeInterval = interval * 3600000;
 
+    var nextTime = currentTime + timeInterval;
+    var date = new Date(nextTime);
+    var nextaDate =  convertDateObjectToDate(date);
+    document.querySelector('.time-mark .-left .--next').innerHTML = nextaDate.time;
+    document.querySelector('#js-next .--date').innerHTML = nextaDate.date;
+}
 
+function convertDateObjectToDate(today){
+        var day = today.getDate();
+        var month = today.getMonth();
+        day = (day < 10) ? '0'+ day : day;
+        month = (month < 10) ? '0'+month :month;
+        var date =day +"-"+month+"-"+ today.getFullYear();
+        var hours = today.getHours();
+        var minutes = today.getMinutes();
+        hours = (hours < 10) ? '0'+ hours : hours;
+        minutes = (minutes < 10 ) ? '0'+ minutes : minutes;
+        var time = hours+':'+minutes;
+    return {date, time};
+}
